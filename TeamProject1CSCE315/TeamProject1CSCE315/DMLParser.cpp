26326ff59e6de49592770cf063d7f20414ed2f25
jsitzman <jsitzman369@neo.tamu.edu>
2014-02-06 09:53:58 -0600

    DMLParser Updated
    Strings can now be parsed (please check comments in DMLParser for
    details before using DMLParser)

--- TeamProject1CSCE315/TeamProject1CSCE315/DMLParser.cpp
+++ TeamProject1CSCE315/TeamProject1CSCE315/DMLParser.cpp
@@ -10,6 +10,30 @@
 All functionality of the parser is here.
 */
 void DMLParser::parse(string &line, string &response) {
+	//I was unsure which of the 2 inputs were actually the "command line". Replace "input" with either "line" or "response"
+	//this code has been tested in a controlled environment, given a command such as "Parse<<This==String&&Please", will
+	//produce "parsedString", the vector, containing { "Parse", "This", "String", "Please" } 
+	string output;
+	size_t found = input.find_first_of(" ,&<="); //READ THE ABOVE COMMENT BEFORE CHANGING THIS ERROR
+	while (found != string::npos)
+	{
+		input[found] = '*';
+		found = input.find_first_of(" ,&<=", found + 1);
+	}
+	stringstream reader(input); //READ THE ABOVE COMMENT BEFORE CHANGING THIS ERROR
+	while (getline(reader, output, '*')){
+		if (output == "") {
+		}
+		else{
+			parsedString.push_back(output);
+		}
+
+	}
+
+	for (int i = 0; i < parsedString.size(); i++) {
+		cout << parsedString[i] << "\n";
+	}
+
 	//split keywords and variables based on terminal characters (i.e.) ' ', '&', '<', '*', ',', etc.
 	//should we use vector<string> or string[]? <---------------------------------------------------
 	//we need a split function either way^	
--- TeamProject1CSCE315/TeamProject1CSCE315/DMLParser.h
+++ TeamProject1CSCE315/TeamProject1CSCE315/DMLParser.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -13,6 +15,7 @@ class DataManager;
 class DMLParser {
 private:
 	DataManager* dataManager;
+	vector<string> parsedString; //contains the parsed input command line
 	void parse(string &line, string &response);
 public:
 	DMLParser(DataManager* dataManager);
