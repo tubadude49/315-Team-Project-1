bff3ec9f27b71667d0c3c00b757ee2eaf298a5c1
thelastares <spam.bateson@gmail.com>
2014-02-11 15:05:41 -0600

    Update DMLParser.cpp

--- TeamProject1CSCE315/TeamProject1CSCE315/DMLParser.cpp
+++ TeamProject1CSCE315/TeamProject1CSCE315/DMLParser.cpp
@@ -9,10 +9,103 @@
 /* 	Main implementation of the DMLParser.
 All functionality of the parser is here.
 */
-void DMLParser::parse(string &line, string &response) {
-	//split keywords and variables based on terminal characters (i.e.) ' ', '&', '<', '*', ',', etc.
-	//should we use vector<string> or string[]? <---------------------------------------------------
-	//we need a split function either way^	
+int DMLParser::parse(string &line) {
+	//add try/catch for bounds checking (std::invalid_argument)
+	vector<string> split = split(line);
+	
+	if(split[0] == "EXIT") { // IMPL
+		return 0x1;
+	}
+	else if(split[0] == "UPDATE") {
+		return 0xE;
+	}
+	else if(split[0] == "SHOW") {
+		return 0xE;
+	}
+	else if(split[0] == "WRITE") {
+		string outputRelation = split[1];
+		return 0xE;
+	}
+	else if(split[0] == "OPEN") {
+		string outputRelation = split[1];
+		return 0xE;		
+	}
+	else if((split[0] == "CREATE") && (split[1] == "TABLE")) { // IMPL
+		//CREATE TABLE
+		string outputRelation = split[2];
+		if(split[3] == "(") {
+			vector<string> tuple;
+			for(int i=4; split[i] != ")";i++) {
+				tuple.push_back(split[i]);
+			}
+			dataManager.insert(outputRelation, tuple);
+			dataManager.addBuildCmd(outputRelation, line);
+		}
+		else {
+			return 0x6;
+		}
+	}
+	else if((split[0] == "INSERT") && (split[1] == "INTO")) {
+		string outputRelation = split[2];
+		if((split[3] == "VALUES") && (split[4] == "FROM")) {
+			if(split[5] == "(") {
+				for(int i=6; split[i] != ")";i++) {
+					
+				}
+			}
+		}
+		else {
+			return 0x4;
+		}
+		return 0xE;
+	}
+	else if((split[0] == "DELETE") && (split[1] == "FROM"))
+	else {
+		string outputRelation = split[0];
+		if(split[1] != "<-") return 0x2;
+		if(split[2] == "project") {
+			//PROJECT
+			return 0xE;
+		}
+		else if(split[2] == "rename") {
+			//RENAME
+			return 0xE;
+		}
+		else if(split[2] == "select") {
+			//SELECT
+			return 0xE;
+		}
+		else {
+			string firstRelation = split[2];
+			string op = split[3];
+			string secondRelation = split[4];
+			if(op == "+") {
+				dataManager.setUnion(firstRelation, secondRelation, outputRelation);
+				dataManager.addBuildCmd(outputRelation, line);				
+				if(split[5] != ";") return 0x5;
+			}
+			else if(op == "-") {
+				dataManager.setDifference(firstRelation, secondRelation, outputRelation);
+				dataManager.addBuildCmd(outputRelation, line);
+				if(split[5] != ";") return 0x5;
+			}
+			else if(op == "*") {
+				dataManager.crossProduct(firstRelation, secondRelation, outputRelation);
+				dataManager.addBuildCmd(outputRelation, line);			
+				if(split[5] != ";") return 0x5;
+			}
+			else if(op == "JOIN") {
+				dataManager.naturalJoin(firstRelation, secondRelation, outputRelation);				
+				dataManager.addBuildCmd(outputRelation, line);
+				if(split[5] != ";") return 0x5;
+			}
+			else {
+				return 0x3;
+			}
+		}
+	}
+	
+	
 	cout << "[DMLParser]: " << line << endl;
 }
 
@@ -23,23 +116,24 @@ DMLParser::DMLParser(DataManager* dataManager) {
 DMLParser::~DMLParser() {
 }
 
-void DMLParser::parseProgram(string &program, string &response) {
-	//split the lines of the program based on semicolons (;)
-	//loop through for loop the lines and parse one at a time
-	//for(;;) {
-	string nuller = "";
-	parse(nuller, response);
-	//}
-	response = "Completed!";
-	// please don't run the above
+int DMLParser::parseProgram(string &program) {
+	vector<string> program = splitPrograms(program);
+	for(string line : program) {
+		int response;
+		if(response = parse(line)) {
+			return response;
+		}
+	}
+	return 0x0;
 }
+
 
 static string terminalChars = " ,&<>=!()"; //TODO
 static string removeChars = " ";
 //static vector<char> terminalChars = { ',', '&', '<', '>', '=', '!', '(', ')' };
 //static vector<char> removeChars = { ' ' };
 
-vector<string> DMLParser::split(string &input){
+vector<string> split(string &input){
 	vector<string> splitString;
 	size_t found = input.find_first_of(terminalChars);
 	while (found != string::npos)
