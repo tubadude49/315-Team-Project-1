72d921960aa3389e157a1f52c3a360a687655a00
thelastares <spam.bateson@gmail.com>
2014-02-17 22:20:57 -0600

    added comments

--- TeamProject1CSCE315/TeamProject1CSCE315/DMLParser.h
+++ TeamProject1CSCE315/TeamProject1CSCE315/DMLParser.h
@@ -14,16 +14,30 @@ using namespace std;
 
 class DataManager;
 
+/* 	Main implementation of the DMLParser.
+All functionality of the parser is here.
+*/
+
 class DMLParser {
 private:
 	DataManager* dataManager;
+	/* Parse the program line by line */
 	int parse(string &line);
-	string getRelation(vector<string> tokens, int startAt, string recursiveRelation);
+	/* Parse a complex select, project, or rename statement */
 	string parseComplex(vector<string> tokens, int startAt, string recursiveRelation);
 public:
+	/* Constructor for DMLParser */
 	DMLParser(DataManager* dataManager);
+	/* Destructor for DMLParser */
 	~DMLParser();
+	/* Parse an arbitrarily large program. 
+	Output returned is the error code number.
+	Refer to implementation for error code reference. */
 	int parseProgram(string &program);
+	/* Split the given program into the multiple statements that comprise it.
+	*/
 	vector<string> splitProgram(string &input);
+	/* Split a given statement into a tokenized vector of strings.
+	*/
 	vector<string> split(string &input);
 };
